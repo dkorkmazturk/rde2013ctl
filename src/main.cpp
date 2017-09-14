93dffbc63a92b156e8b488ddad82e535131cbc0f
Dogukan Korkmazturk <d.korkmazturk@gmail.com>
2017-09-14 09:44:37 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+#include "rde2013.h"
+
+Command processArgs(const std::vector<std::string> &args);
+
+int main(int argc, char *argv[])
+{
+    std::vector<std::string> args(argv + 1, argv + argc);
+    try {
+        RDE2013::getInstance().sendCommand(processArgs(args));
+    } catch (const std::runtime_error &err) {
+        std::cerr << err.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+Command processArgs(const std::vector<std::string> &args)
+{
+    if(args.size() == 3)
+    {
+        if(args[0] == "set")
+        {
+            if(args[1] == "wheel-light")
+            {
+                if(args[2] == "on")
+                    return Command::TURN_WHEEL_LIGHT_ON;
+                else if(args[2] == "off")
+                    return Command::TURN_WHEEL_LIGHT_OFF;
+            }
+
+            else if(args[1] == "logo-light")
+            {
+                if(args[2] == "on")
+                    return Command::TURN_LOGO_LIGHT_ON;
+                else if(args[2] == "off")
+                    return Command::TURN_LOGO_LIGHT_OFF;
+            }
+        }
+    }
+
+    throw std::runtime_error("Invalid command line argument");
+}
---
+++ src/rde2013.h
@@ -0,0 +1,32 @@
+#ifndef RDE2013_H
+#define RDE2013_H
+
+#include <libusb-1.0/libusb.h>
+#include <stdexcept>
+
+// RAII Style, noncopyable, singleton wrapper for needed libusb functionality
+
+enum class Command {TURN_WHEEL_LIGHT_ON, TURN_WHEEL_LIGHT_OFF, TURN_LOGO_LIGHT_ON, TURN_LOGO_LIGHT_OFF};
+
+class RDE2013
+{
+private:
+    explicit RDE2013();
+    RDE2013(const RDE2013 &) {}
+    RDE2013& operator=(const RDE2013 &) {}
+    ~RDE2013();
+
+    void sendCommand(unsigned char * const command);
+
+    const int usb_interface = 0;
+    libusb_context *lusb_context;
+    libusb_device_handle *dev_handle;
+    bool kernelDriverWasAttached;
+
+public:
+    static RDE2013 &getInstance();
+    void sendCommand(const Command &command);
+
+};
+
+#endif // RDE2013_H
